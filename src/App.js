b00a29922c646c38f6c7704ab21c26f2860d7669
Marek Mroz <marekmroz@wp.pl>
2019-04-03 11:40:44 +0200

    właściwości klasy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Counter from './Counter'
-
+import Counter from './Counter';
 
 const App = (props) => (
   <div>
-  
+    <Counter />
+    <Counter />
   </div>
 )
 
-export default App;
+export default App
--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,5 +1,30 @@
 import React from 'react'
 
+class Counter extends React.Component {
+    
+    state = {
+        number: 10,
+    }
 
+    inc = () => {
+        this.setState({number: this.state.number + 1})
+    }
 
-export default Counter;
+    render() {   
+        return (
+            <div>
+              <h1>
+               {this.state.number}
+              </h1>
+
+              <button
+               onClick = {this.inc}
+              >
+                 +
+              </button>
+            </div>
+        )
+    }
+}
+
+export default Counter
